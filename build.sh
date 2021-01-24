4c94c01e84bea6e6d40c33ad71a32985a4f23513
prooholic <peterardiarta11@gmail.com>
2021-01-24 07:56:05 +0000

    Import compilation scripts

--- build.sh
+++ build.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
-export ARCH=arm64
-export SUBARCH=arm64
-export KBUILD_BUILD_USER=iqbal
-export KBUILD_BUILD_HOST=predator
-export CROSS_COMPILE=/home/iqbal/gcc64/bin/aarch64-linux-gnu-
-export CROSS_COMPILE_ARM32=/home/iqbal/gcc32/bin/arm-linux-gnueabi-
-mkdir -p out
-make O=out clean
-make O=out mrproper
-make O=out predator_defconfig
-make O=out -j$(nproc --all)
+
+export KERNELNAME="CK"
+
+export LOCALVERSION=0.2
+
+export KBUILD_BUILD_USER=Peter
+
+export KBUILD_BUILD_HOST=DroneCI
+
+export TOOLCHAIN=clang
+
+export DEVICES=whyred
+
+source helper
+
+gen_toolchain
+
+send_msg "Start building ${KERNELNAME} ${LOCALVERSION} for ${DEVICES}..."
+
+send_msg "Starting building stock version..."
+
+START=$(date +"%s")
+
+for i in ${DEVICES//,/ }
+do
+	build ${i} -oldcam -stock
+
+	build ${i} -newcam -stock
+done
+
+END=$(date +"%s")
+
+DIFF=$(( END - START ))
+
+send_msg "Build succesfully in $((DIFF / 60))m $((DIFF % 60))s"
